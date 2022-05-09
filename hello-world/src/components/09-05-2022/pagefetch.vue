129e0105d9b8a01f30f59f107b9dae7471ee7fa8
Lavanya-500apps <lavanya.gavara@500apps.com>
2022-05-09 19:02:53 +0530

    commit

--- hello-world/src/components/09-05-2022/pagefetch.vue
+++ hello-world/src/components/09-05-2022/pagefetch.vue
@@ -7,8 +7,11 @@
       aria-controls="my-table"
     ></b-pagination>
      <b-tabs>
-      <b-tab title="Tab 1" @click="func()" active>
+      <b-tab title="Tab 1" @click="func1()" active>
 <b-table :items="res" :fields="fields" :per-page="perPage" :current-page="currentPage"></b-table>
+      </b-tab>
+      <b-tab title="Tab 2" @click="func2()" active>
+<b-table :items="result" :fields="fields" :per-page="perPage" :current-page="currentPage"></b-table>
       </b-tab>
      </b-tabs>
       
@@ -23,18 +26,25 @@ data(){
 perPage:3,
 currentPage:1,
 res:" ",
+result:" ",
 
     }
 },
 methods:{
-async func(){
+async func1(){
     let response=await fetch("https://jsonplaceholder.typicode.com/todos")
     this.res= await response.json()
+},
+async func2(){
+    let response=await fetch("http://universities.hipolabs.com/search?country=")
+    this.result= await response.json()
 }
 },
 computed:{
     rows(){
        return this.res.length; 
+      // return this.result.length;
+       
     }
 }
 };
