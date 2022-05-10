258ab9d3d923dd7f5076c490b83651f456864bd5
Lavanya-500apps <lavanya.gavara@500apps.com>
2022-05-10 19:33:21 +0530

    commit

---
+++ hello-world/src/components/10-05-2022/test6.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    lowercase :<br> <input type="text" v-model="lower" placeholder="Enter lowercase"/><br />
+
+    uppercase :<br> <input type="text" v-model="upper"/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "UppeR",
+
+  data() {
+    return {
+      lower: "",
+
+      upper: "",
+    };
+  },
+
+  watch: {
+    lower: function (val) {
+      this.lower = val;
+
+      this.upper = this.lower.toUpperCase();
+    },
+  },
+};
+</script>
+
+<style>
+</style>
