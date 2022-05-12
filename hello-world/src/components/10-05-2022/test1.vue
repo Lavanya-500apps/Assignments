db2ccb08f421ee9277f21e923e42c7e55cf41ae7
Lavanya-500apps <lavanya.gavara@500apps.com>
2022-05-12 18:09:42 +0530

    12-05

---
+++ hello-world/src/components/10-05-2022/test1.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <p>checking type of object is string</p>
+    <button @click="fun()">click</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TEst1",
+  data() {
+    return {
+      data: [{ name: "lavanya", id: 1199, company: "mantra" }],
+      str: "hello",
+      str1: 21,
+    };
+  },
+  methods: {
+    fun() {
+      if (typeof this.data["name"] === "string") {
+        //if(typeof this.str==='string')
+        //if(typeof this.str1==='string')
+        document.write("it is a string");
+        console.log("it is  a string");
+      } else {
+        document.write("it is not a string");
+        console.log("it is not a string");
+      }
+    },
+  },
+};
+</script>
+
+<style>
+</style>
--- hello-world/src/components/26-04-2022/demo.vue
+++ hello-world/src/components/26-04-2022/demo.vue
@@ -3,14 +3,14 @@
 <h1>heading tags</h1>
 <p>paragraphs</p>
 <p>{{value}}</p>
-<button type="button" @click="f1()">click</button>
+<button @click="f1()">click</button>
 
       </div>
 </template>
 
 <script>
 export default {
-    name:DEmo,
+    name:"DEmo",
     data() {
         return{
             array: [1,2,3,4,5],
@@ -20,8 +20,9 @@ export default {
     methods:
     {
       f1() {
-          this.value=this.array.forEach((arr)=>
-          console.log(arr))
+          this.value=this.array.forEach((arr)=>{
+            
+          console.log(arr*2)})
 
       },
 
